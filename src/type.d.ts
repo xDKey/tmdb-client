ccb0b0d2648b5d164edafc8e64ece9e9de741155
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-15 18:45:10 +0300

    chore(types): add several types for TS

---
+++ src/type.d.ts
@@ -0,0 +1,33 @@
+export type Movie = {
+  id: number
+  title: string
+  originalTitle: string
+  poster: string
+  overview: string
+  voteAverage: number
+  voteCount: number
+  releaseDate: string
+  genres: Array<number>
+}
+
+export type Action = {
+  type: string
+  payload?: any
+}
+
+export type RawMovie = {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: Array<number>
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
