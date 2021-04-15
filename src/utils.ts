7d77ae0698bd69204358e335f49771840a699290
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-15 18:46:45 +0300

    feat(fetch): add some functions for get data from api

---
+++ src/utils.ts
@@ -0,0 +1,45 @@
+import { Movie, RawMovie } from './type'
+
+const baseUrl = 'https://api.themoviedb.org/3/'
+const apikey = '?api_key=314e6dde9895e914a26e321ea921a444'
+const language = '&language=ru-RU'
+const posterPath = 'https://image.tmdb.org/t/p/w500/'
+
+const getResource = async (url: string) => {
+  return await fetch(url).then((data) => data.json())
+}
+
+const movieFormating = ({
+  id,
+  title,
+  original_title,
+  overview,
+  poster_path,
+  release_date,
+  vote_average,
+  vote_count,
+  genre_ids,
+}: RawMovie): Movie => ({
+  id,
+  title,
+  overview,
+  originalTitle: original_title,
+  poster: posterPath + poster_path,
+  voteAverage: vote_average,
+  voteCount: vote_count,
+  releaseDate: release_date,
+  genres: genre_ids
+})
+
+export const findMovie = async (id: number) => {
+  const url = baseUrl + 'movie/' + id + apikey + language
+  const res = await getResource(url)
+  return movieFormating(res)
+}
+
+export const discoverMovie = async () => {
+  const url = baseUrl + 'discover/movie' + apikey + language
+  const res = await getResource(url)
+  const result = res.results.map((movie: RawMovie) => movieFormating(movie))
+  return await result
+}
