e6c21001f6b975bd48c51ce9d04c4bb621c71147
Vladimir Mironov <vladimir.mironov@jetruby.com>
2021-04-15 15:09:07 +0300

    project init

---
+++ src/components/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const App = () => {
+  return <h1>Start</h1>
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+}
+
+module.exports = (env, argv) => {
+  if (argv && argv.mode === 'production')
+    config.output.publicPath = '/todoapp-typescript/'
+
+  return config
+}
